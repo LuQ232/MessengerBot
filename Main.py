34226d99383714ec82c287fa0aae7208517b2e47
LuQ232 <LuQ232>
2020-02-16 23:07:28 +0100

    Lekkie poprawki, dodano reakcje na wiadomosc.

--- Main.py
+++ Main.py
@@ -21,50 +21,49 @@ def zwroc_haslo():
     return lista[1]
 
 
-class Bot:
-    def zaloguj(self,login,haslo):
-        global client
-        client = Client(login, haslo)
+class Bot(Client):
 
     def wyloguj(self):
-        client.logout()
+        self.logout()
 
     def wyslij_wiadomosc(self,wiadomosc,odbiorca,typ_konwersacji):
-            client.send(Message(text=wiadomosc), thread_id=odbiorca, thread_type=typ_konwersacji)
+            self.send(Message(text=wiadomosc), thread_id=odbiorca, thread_type=typ_konwersacji)
 
     def pobierz_wszystkie_konwersacje(self):
-        konwersacje = client.fetchAllUsers()
+        konwersacje = self.fetchAllUsers()
         return konwersacje
 
     def pobierz_najczestsze_konwersacje(self):
-        konwersacje = client.fetchThreadList()
+        konwersacje = self.fetchThreadList()
         return konwersacje
+    def reaguj_na_wiadomosc(self,bodziec,odpowiedz):
+        try:
+            while True:
+
+                for osoba in self.pobierz_najczestsze_konwersacje():
+                    Wiadomosci = self.fetchThreadMessages(thread_id=osoba.uid, limit=1)
+                    for wiadomosc in Wiadomosci:
+                        if wiadomosc.text == bodziec:
+                           self.wyslij_wiadomosc(odpowiedz, osoba.uid, osoba.type)
+                        else:
+                            pass
+                time.sleep(1)
+                print("Pracuje")
+        except KeyboardInterrupt:
+            print('interrupted!')
+
+
+
+
 
 class Uzytkownik:
     pass
 
 
+Moj_Bot=Bot(zwroc_login(),zwroc_haslo())
 
 
-Moj_Bot=Bot()
-Moj_Bot.zaloguj(zwroc_login(),zwroc_haslo())
-
-
-
-try:
-    while True:
-
-        for osoba in Moj_Bot.pobierz_najczestsze_konwersacje():
-            Wiadomosci = client.fetchThreadMessages(thread_id=osoba.uid, limit=1)
-            for wiadomosc in Wiadomosci:
-                if wiadomosc.text == "jestem super":
-                    Moj_Bot.wyslij_wiadomosc("dokladnie tak tak tak tak :)))))", osoba.uid, osoba.type)
-                else:
-                    pass
-        time.sleep(1)
-        print("Pracuje")
-except KeyboardInterrupt:
-    print('interrupted!')
+Moj_Bot.reaguj_na_wiadomosc("Witam","Hej")
 
 Moj_Bot.wyloguj()
 
