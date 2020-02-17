e84b56de261830d0cc77573b0fbaf7b0378b44cc
LuQ232 <LuQ232>
2020-02-17 14:31:59 +0100

    Downloading and sending memes from internet sites

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from fbchat.models import *
 import requests
 from bs4 import BeautifulSoup
 
+
 def czytaj_plik():
     f = open("TEST.txt", "r")
     if f.mode== 'r':
@@ -33,6 +34,33 @@ class Bot(Client):
         url= new_str.split('" ')[0]
         return url
 
+    def random_jeja_url(self):
+        page = requests.get("https://memy.jeja.pl/losowe")
+        soup = BeautifulSoup(page.content, 'html.parser')
+        jeja_div = soup.find(class_='ob-left-images')
+        jeja_div_to_str = jeja_div.__str__()
+        new_str = jeja_div_to_str.split('src="')[1]
+        url= new_str.split('"')[0]
+        return url
+
+    def random_mistrzowe_url(self):
+        page = requests.get("https://mistrzowie.org/losuj")
+        soup = BeautifulSoup(page.content, 'html.parser')
+        jeja_div = soup.find(class_='pic_wrapper')
+        jeja_div_to_str = jeja_div.__str__()
+        new_str = jeja_div_to_str.split('src="')[1]
+        url= new_str.split('" ')[0]
+        url= "https://mistrzowie.org"+url
+        return url
+    def random_jajco_url(self):
+        page = requests.get("http://jajco.pl/rand")
+        soup = BeautifulSoup(page.content, 'html.parser')
+        jeja_div = soup.find(class_='imgcut')
+        jeja_div_to_str = jeja_div.__str__()
+        new_str = jeja_div_to_str.split('src="')[1]
+        url= new_str.split('"')[0]
+        return url
+
     def onMessage(self, mid=None, author_id=None, message=None, message_object=None, thread_id=None,
                   thread_type=ThreadType.USER, ts=None, metadata=None, msg=None):
         if author_id == self.uid:
@@ -42,10 +70,16 @@ class Bot(Client):
         elif message.lower() =="spierdalaj":
             self.sendMessage("Sam spierdalaj!!", thread_id, thread_type)
         elif message.lower() =="!demotywator":
-            self.sendRemoteFiles(self.random_demotywator_url(),message,thread_id,thread_type)
+            self.sendRemoteFiles(self.random_demotywator_url(),None,thread_id,thread_type)
+        elif message.lower() == "!jeja":
+            self.sendRemoteFiles(self.random_jeja_url(), None, thread_id, thread_type)
+        elif message.lower() == "!mistrzowie":
+            self.sendRemoteFiles(self.random_mistrzowe_url(), None, thread_id, thread_type)
+        elif message.lower() == "!jajco":
+            self.sendRemoteFiles(self.random_jajco_url(), None, thread_id, thread_type)
 
 Moj_Bot=Bot(zwroc_login(),zwroc_haslo())
-Moj_Bot.random_demotywator_url()
 Moj_Bot.listen()
+
 
 
